0465a7bb413517e8e01ed4751342c2c40ef4c223
DESKTOP-A0AE9UD\HP <simarjots9@gmail.com>
2021-03-25 17:19:06 +0530

    home screen

---
+++ src/wrappers/MainWrapper.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState, useContext } from "react";
+// import ApplicationRouter from "../router/applicationRouter";
+import { Container } from "react-bootstrap";
+import Header from "../components/Header/Header";
+import Loader from "../components/Loader/Loader";
+
+const MainWrapper = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+     
+  }, []);
+   
+  
+  return (
+    <div className="appMain">
+    
+     
+    </div>
+  );
+};
+
+export default MainWrapper;
