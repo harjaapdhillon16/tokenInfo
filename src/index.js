15a37b4e0c8a08ed8a1bc4eb950450d555f1b002
DESKTOP-A0AE9UD\HP <simarjots9@gmail.com>
2021-03-31 12:00:58 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
+import { BrowserRouter} from "react-router-dom";
 
 Amplify.configure(awsconfig);
 
