21410cd8ac118b055420eb3ae506c5d6de7db6ed
Faisal Arshed <faisalarshed28@gmail.com>
2021-05-12 14:37:57 +0500

    Changes to api

--- src/index.js
+++ src/index.js
@@ -4,20 +4,18 @@ import './index.css';
 import App from './App';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { BrowserRouter} from "react-router-dom"; 
-import {AppContext} from "./context/appContext"
-
+import { BrowserRouter } from 'react-router-dom';
+import { AppContext } from './context/appContext';
 
 Amplify.configure(awsconfig);
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-		  <AppContext >
-			<App />
+			<AppContext>
+				<App />
 			</AppContext>
 		</BrowserRouter>
-
 	</React.StrictMode>,
 	document.getElementById('root')
 );
