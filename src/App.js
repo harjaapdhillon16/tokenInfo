0658083b796fdda2af9ce8e971c9bf4cd85101f6
Faisal Arshed <faisalarshed28@gmail.com>
2021-05-21 00:35:11 +0500

    Create agent on signin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import { Hub, API, graphqlOperation } from 'aws-amplify';
+import { createAgent } from './graphql/mutations';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainWrapper from './wrappers/MainWrapper';
 import FormController from './views/FormController';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
+Hub.listen('auth', (data) => {
+	const { sub, email } = data.payload.data.attributes;
+	let attributes = { id: sub, email };
+	switch (data.payload.event) {
+		case 'signIn':
+			API.graphql(
+				graphqlOperation(createAgent, {
+					input: attributes,
+					condition: { id: { attribute_not_exists: attributes.id } }
+				})
+			).catch(() => {});
+			break;
+		default:
+			break;
+	}
+});
 
 function App() {
 	return (
